3e709b97eb4f70244c81d2ad9dfe3196c2c7bfbe
tarekzemmouri <zemmouri.tarek@hotmail.com>
2021-08-02 23:14:30 +0200

    Second commit for team/contact

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -1,7 +1,23 @@
 class StaticController < ApplicationController
+  attr_accessor :id
+
+ 
+
   def home
+    @random = rand(1000)
+    puts "$" *60
+    puts "mon numéro random #{@random}"
+    puts "$"*60
   end
 
   def home_fr
   end
+
+  def team
+
+  end
+
+  def contact
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
+  #get 'static/team'
+  #get 'static/contract'
+  get '/', to: 'static#home'
   get 'home', to: 'static#home'
   get 'maison', to: 'static#home_fr'
+  get 'team', to: 'static#team'
+  get 'contact', to: 'static#contact'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
